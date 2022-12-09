fef5a647cd0d147b16ff4341ad9343f67a1f76f2
davidfan168 <davidfan168@hotmail.com>
2022-12-09 18:11:43 -0500

    bug fix

--- Game.cpp
+++ Game.cpp
@@ -959,7 +959,7 @@ void Game::update_kill_clones(float elapsed) {
 			player_1_clone_cnt++;
 		}
 	}
-	
+
 	if (player_0_clone_cnt == 0 || player_1_clone_cnt == 0) {
 		std::cout << "All clones killed. Game Over\n";
 		state = GameOver;
@@ -1175,12 +1175,12 @@ MESSAGE Game::recv_message(Connection *connection_, Player *client_player, bool 
 			else {
 				// adjust character position for network latency for clients
 				// server will do this later (after it relays the message) to prevent double counting
-				std::chrono::time_point<std::chrono::system_clock> client_tp = client_player->time_updated;
-				std::chrono::duration latency = std::chrono::system_clock::now() - client_tp;
-				std::chrono::duration<float> f_latency = latency;
-				std::cout << "latency is: " << f_latency.count() << "\n";
-				glm::vec2 displacement = client_player->get_direction() * f_latency.count() * PLAYER_SPEED;
-				common_data->characters[client_player->player_id].move_character(displacement.x, displacement.y);
+				// std::chrono::time_point<std::chrono::system_clock> client_tp = client_player->time_updated;
+				// std::chrono::duration latency = std::chrono::system_clock::now() - client_tp;
+				// std::chrono::duration<float> f_latency = latency;
+				// std::cout << "latency is: " << f_latency.count() << "\n";
+				// glm::vec2 displacement = client_player->get_direction() * f_latency.count() * PLAYER_SPEED;
+				// common_data->characters[client_player->player_id].move_character(displacement.x, displacement.y);
 			}
 			break;
 		}
@@ -1228,12 +1228,12 @@ void Game::process_action(Player *player, MESSAGE message_type) {
 		case MESSAGE::PLAYER_UPDATE: {
 			// TODO: extract it to helper function
 			// adjust character position for network latency
-			std::chrono::time_point<std::chrono::system_clock> client_tp = player->time_updated;
-			std::chrono::duration latency = std::chrono::system_clock::now() - client_tp;
-			std::chrono::duration<float> f_latency = latency;
-			std::cout << "latency is: " << f_latency.count() << "\n";
-			glm::vec2 displacement = player->get_direction() * f_latency.count() * PLAYER_SPEED;
-			common_data->characters[player->player_id].move_character(displacement.x, displacement.y);
+			// std::chrono::time_point<std::chrono::system_clock> client_tp = player->time_updated;
+			// std::chrono::duration latency = std::chrono::system_clock::now() - client_tp;
+			// std::chrono::duration<float> f_latency = latency;
+			// std::cout << "latency is: " << f_latency.count() << "\n";
+			// glm::vec2 displacement = player->get_direction() * f_latency.count() * PLAYER_SPEED;
+			// common_data->characters[player->player_id].move_character(displacement.x, displacement.y);
 			break;
 		}
 		default:
